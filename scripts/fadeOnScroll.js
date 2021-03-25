166cac8a42fd6004a62313609a17dc3c56d27f6e
Daniel Karlkvist <danielkarlkvist@gmail.com>
2021-03-25 17:57:35 +0100

    Cleaned up code

--- scripts/fadeOnScroll.js
+++ scripts/fadeOnScroll.js
@@ -1,27 +1,15 @@
-/* $(function() { // $(document).ready shorthand
-    $('.monster').fadeIn('slow');
-}); */
-
 $(document).ready(function() {
-
-    /* Every time the window is scrolled ... */
+    /* W4indow is scrolled */
     $(".scroll-snap-container").scroll(function() {
-
         /* Check the location of each desired element */
         $('.fade-on-scroll-element').each(function() {
-
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            var bottomOfObject = $(this).position().top + $(this).outerHeight();
+            var bottomOfWindow = $(window).scrollTop() + $(window).height();
 
             /* If the object is completely visible in the window, fade it in */
-            if (bottom_of_window > bottom_of_object) {
-
+            if (bottomOfWindow > bottomOfObject) {
                 $(this).animate({ 'opacity': '1' }, 500);
-
             }
-
         });
-
     });
-
 });
